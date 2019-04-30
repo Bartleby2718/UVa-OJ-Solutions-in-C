638e9f687f371a5bfc1850bd90a18c07fc1f8d84
Bartleby <bartleby2718@gmail.com>
2019-04-30 16:33:30 +0900

    12503 - Robot Instructions

---
+++ 12503/submission.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int getDisplacement(int *instructions, char *instruction)
+{
+    int displacement, oneBasedIndex;
+    if (strcmp(instruction, "LEFT") == 0)
+    {
+        displacement = -1;
+    }
+    else if (strcmp(instruction, "RIGHT") == 0)
+    {
+        displacement = 1;
+    }
+    else
+    {
+        sscanf(instruction, "SAME AS %d", &oneBasedIndex);
+        displacement = instructions[oneBasedIndex - 1];
+    }
+    return displacement;
+}
+
+int main()
+{
+    unsigned numTestCases, i; /* T <= 100*/
+    scanf("%u", &numTestCases);
+    for (i = 0; i < numTestCases; ++i)
+    {
+        int position = 0;
+        unsigned numInstructions, j; /* 1 <= n <= 100 */
+        scanf("%u\n", &numInstructions);
+        int instructions[numInstructions];
+        for (j = 0; j < numInstructions; ++j)
+        {
+            char line[12] = "";
+            fgets(line, 12, stdin);
+            strtok(line, "\n");
+            int displacement = getDisplacement(instructions, line);
+            instructions[j] = displacement;
+            position += displacement;
+        }
+        printf("%d\n", position);
+    }
+
+
+    return 0;
+}
