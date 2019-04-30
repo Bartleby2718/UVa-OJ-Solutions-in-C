bd0e71adf1b1974b73e254f329307e548664fe3f
Bartleby <bartleby2718@gmail.com>
2019-04-30 23:32:50 +0900

    12478

---
+++ 12478/main.c
@@ -0,0 +1,116 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_NAMES 8
+#define MAX_STRING_LENGTH 8
+#define GRID_WIDTH 9
+#define GRID_LENGTH 9
+
+
+void insertionSort(char *array, unsigned length)
+{
+    int i, j;
+    for (i = 1; i < length; ++i)
+    {
+        char key = array[i];
+        for (j = i - 1; array[j] > key && j >= 0; --j)
+        {
+            array[j + 1] = array[j];
+        }
+        array[j + 1] = key;
+    }
+}
+
+bool equivalentWhenPermuted(char *string1, char *string2, unsigned length)
+{
+    char *first = calloc(length + 1, sizeof(char));
+    strncpy(first, string1, length);
+    insertionSort(first, length);
+    char *second = calloc(length + 1, sizeof(char));
+    strncpy(second, string2, length);
+    insertionSort(second, length);
+    bool result = strcmp(first, second) == 0;
+    free(first);
+    free(second);
+    return result;
+}
+
+void testEquivalentWhenPermuted(char *string1, char *string2, unsigned length, bool expected)
+{
+    bool actual = equivalentWhenPermuted(string1, string2, length);
+    printf("%s & %s: expected %s, got %s.\n", string1, string2, expected ? "true" : "false", actual ? "true" : "false");
+}
+
+int main()
+{
+    /*
+    testEquivalentWhenPermuted("acbb", "cbca", 4, false);
+    testEquivalentWhenPermuted("acbc", "bcca", 4, true);
+    testEquivalentWhenPermuted("aaaaaaaaaaaaaaaaaaaab", "aaaaaaaaaabaaaaaaaaaa", 21, true);
+    return 0;
+     */
+    char names[NUM_NAMES][MAX_STRING_LENGTH] = {"RAKIBUL",
+                                                "ANINDYA",
+                                                "MOSHIUR",
+                                                "SHIPLU",
+                                                "KABIR",
+                                                "SUNNY",
+                                                "OBAIDA",
+                                                "WASI"};
+    char grid[GRID_LENGTH][GRID_WIDTH + 1] = {"OBIDAIBKR",
+                                              "RKAULHISP",
+                                              "SADIYANNO",
+                                              "HEISAWHIA",
+                                              "IRAKIBULS",
+                                              "MFBINTRNO",
+                                              "UTOYZIFAH",
+                                              "LEBSYNUNE",
+                                              "EMOTIONAL"};
+    unsigned i, j, k;
+    bool keepSearching = true;
+    for (i = 0; i < NUM_NAMES && keepSearching; ++i)
+    {
+        char *name = names[i];
+        unsigned length = strlen(name);
+        unsigned count = 0;
+        /* check horizontal*/
+        for (j = 0; j < GRID_LENGTH; ++j)
+        {
+
+            for (k = 0; k <= GRID_WIDTH - length; ++k)
+            {
+                char horizontalWord[MAX_STRING_LENGTH] = "";
+                strncpy(horizontalWord, grid[j] + k, length);
+                if (equivalentWhenPermuted(horizontalWord, name, length))
+                {
+                    ++count;
+                }
+            }
+        }
+        /* check vertical */
+        for (j = 0; j <= GRID_LENGTH - length; ++j)
+        {
+            for (k = 0; k < GRID_WIDTH; ++k)
+            {
+                char verticalWord[MAX_STRING_LENGTH] = "";
+                unsigned l;
+                for (l = 0; l < length; ++l)
+                {
+                    verticalWord[l] = grid[j + l][k];
+                }
+                if (equivalentWhenPermuted(verticalWord, name, length))
+                {
+                    ++count;
+                }
+            }
+        }
+        if (count == 2)
+        {
+            printf("%s\n", name);
+            keepSearching = (count != 2);
+        }
+    }
+    return 0;
+}
